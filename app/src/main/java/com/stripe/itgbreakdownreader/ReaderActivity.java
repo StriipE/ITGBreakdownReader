d39552a38982f9dfad0774f4c09e798c78744005
Ewen Auffret <auffret.ewen@gmail.com>
2017-09-23 02:14:42 +0200

    Song bpm is now supported, added stream/break status on display

--- app/src/main/java/com/stripe/itgbreakdownreader/ReaderActivity.java
+++ app/src/main/java/com/stripe/itgbreakdownreader/ReaderActivity.java
@@ -9,6 +9,10 @@ public class ReaderActivity extends AppCompatActivity {
 
     private BreakdownReader breakdownReader;
     private TextView currentMesuresDisplay;
+    private TextView songNameDisplay;
+    private TextView songBPMDisplay;
+    private TextView currentStatus;
+
     private int mesuresLeftValue = 0;
     private boolean displaying = false;
 
@@ -16,7 +20,14 @@ public class ReaderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reader);
 
-        readBreakdown("15 15 (7) 15");
+        songNameDisplay = (TextView) findViewById(R.id.SongName);
+        songBPMDisplay = (TextView) findViewById(R.id.bpmValue);
+        currentStatus = (TextView) findViewById(R.id.currentStatus);
+
+        songNameDisplay.setText("Division 4");
+        songBPMDisplay.setText("180");
+
+        readBreakdown("15 15 32 (16) 16 (32) 16 (26) 64");
         displayStream();
 
     }
@@ -29,6 +40,10 @@ public class ReaderActivity extends AppCompatActivity {
     private void displayStream()
     {
         currentMesuresDisplay = (TextView) findViewById(R.id.currentMesures);
+        int bpm = Integer.parseInt(songBPMDisplay.getText().toString());
+
+        final long mesureDuration = 4 * 60000 / bpm;
+
         final Handler handler = new Handler();
         Runnable streamCountdown = new Runnable() {
             @Override
@@ -38,25 +53,28 @@ public class ReaderActivity extends AppCompatActivity {
                    if (BreakdownReader.isBreak(nextBlock))
                    {
                        mesuresLeftValue = BreakdownReader.getBreakMesures(nextBlock);
-                       handler.postDelayed(this, 200);
+                       mesuresLeftValue--; // Offset used because breaks are written in full duration
+                       currentStatus.setText("Break");
+                       handler.postDelayed(this, mesureDuration);
                        System.out.println("---- New pause : " + mesuresLeftValue + " mesures ----");
                    }
                    else
                    {
                        mesuresLeftValue = BreakdownReader.getNextMesures(nextBlock);
-                       handler.postDelayed(this, 200);
+                       currentStatus.setText("Stream");
+                       handler.postDelayed(this, mesureDuration);
                        System.out.println("---- New stream : " + mesuresLeftValue + " mesures ----");
                    }
                }
                else if (mesuresLeftValue > 0)
                {
                     mesuresLeftValue--;
-                    handler.postDelayed(this, 200);
+                    handler.postDelayed(this, mesureDuration);
                     System.out.println("----" + mesuresLeftValue + " mesures left ----");
                }
                else
                {
-                    handler.removeCallbacks(this);
+                   handler.removeCallbacks(this);
                    System.out.println("End of song");
                }
 
